9ff7e0260e2a4d191441d39231599629a52409e9
a.student on Pristine Lubuntu <a.student@example.com>
2019-04-25 20:37:21 -0700

    finished es6 functions

---
+++ week04-ecmascript/work.js
@@ -0,0 +1,67 @@
+// Immediately invoked Ecmascript6 function
+(() => {
+    const mathResult = (4 + 6) * 12;
+    console.log('Mathresult = ' + mathResult);
+})();
+
+// Immediately invoked function to display an array
+((members) => {
+    members.forEach(function (member) {
+        console.log(member);
+    })
+})([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+
+// Arrays and for...of
+const primeNumbers = (numbers) => {
+    for(let number of numbers) {
+        console.log(number);
+    }
+};
+
+primeNumbers([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]);
+
+// Property Enumeration using for...in
+const charlie = {
+    firstName: 'Charlie',
+    lastName: 'Calvert',
+    address: '3000 Landerholm Cir SE',
+    city: 'Bellevue',
+    state: 'Washington',
+    zip: '98007'
+}
+
+const displayAddress = (charlie) => {
+    for(let property in charlie) {
+        if(charlie.hasOwnProperty(property)) {
+            console.log(property + ": " + charlie[property]);
+        }
+    }
+};
+
+displayAddress(charlie);
+
+// ES6 Class
+class Calculator {
+
+    add(operanda, operandb) {
+        return operanda + operandb;
+    }
+
+    subtract(operanda, operandb) {
+        return operanda - operandb;
+    }
+
+    multiply(operanda, operandb) {
+        return operanda * operandb;
+    }
+
+    divide(operanda, operandb) {
+        return operanda / operandb;
+    }
+}
+
+const calculator = new Calculator();
+console.log(calculator.add(2, 3));
+console.log(calculator.subtract(5, 2));
+console.log(calculator.multiply(3, 5));
+console.log(calculator.divide(15, 3));
