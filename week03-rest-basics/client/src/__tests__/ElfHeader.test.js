390662eeaf9e4f1015e9f6a001dd06909befc6d8
a.student on Pristine Lubuntu <a.student@example.com>
2019-05-10 14:42:30 -0700

    most tests pass

--- week03-rest-basics/client/src/__tests__/ElfHeader.test.js
+++ week03-rest-basics/client/src/__tests__/ElfHeader.test.js
@@ -5,13 +5,14 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ElfHeader from '../components/ElfHeader';
 import ElfDebugEnzyme from '../ElfDebugEnzyme';
+import { BrowserRouter as Router } from 'react-router-dom';
 configure({ adapter: new Adapter() });
 
 const elfDebugEnzyme = new ElfDebugEnzyme(true, 'App.test.js');
 describe('ElfHeader Tests', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<ElfHeader />, div);
+        ReactDOM.render(<Router><ElfHeader /></Router>, div);
         ReactDOM.unmountComponentAtNode(div);
     });
 });
